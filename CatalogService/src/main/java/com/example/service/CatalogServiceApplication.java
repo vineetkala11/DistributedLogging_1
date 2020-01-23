d258ee9d560e584b59622aa3203c0954940f7247
vineet <vineetkala11@gmail.com>
2020-01-23 07:39:48 +0530

    distributed logging kafka
    distributed logging kafka

--- CatalogService/src/main/java/com/example/service/CatalogServiceApplication.java
+++ CatalogService/src/main/java/com/example/service/CatalogServiceApplication.java
@@ -1,9 +1,6 @@
 package com.example.service;
 
-import com.rabbitmq.client.ConnectionFactory;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
